593593d205ac7c39d6a402a9eb32d03716abde18
SaBRe_ <72501036+iHas3Arms@users.noreply.github.com>
2021-09-24 23:55:08 +0100

    Update script.js

--- src/script.js
+++ src/script.js
@@ -2,36 +2,51 @@
 let timer = 5;
 let clicks = 0;
 let clicked = false;
-
-// Elements
-let btn = document.getElementById("btn");
+// Functions
 
 function addClick() {
     if (!clicked) {
+        document.getElementById("btn").innerHTML = "";
         clicks = 1;
         clicked = true;
-    } else {
-        clicks++;
+        return;
     }
+    clicks++;
+    console.log(clicks);
 }
 
 function reset() {
+    document.getElementById("btn").innerHTML = "CLICK TO START PLAYING";
     timer = 5;
     clicks = 0;
     clicked = false;
 }
 
-function addSec() {
+function removeSec() {
     if (clicked) {
         timer--;
     }
 }
 
 function checkTimer() {
-    if (timer == 0) {
-        alert(`You performed ${clicks} clicks in 5 seconds`);
+    if (timer <= 0) {
+        alert(`You performed ${clicks} clicks in 5 seconds\n`
+        + `Your CPS was ${parseFloat(clicks/5).toFixed(1)}`);
         reset();
     }
 }
 
-setInterval(addSec, 1000);
+function update() {
+    checkTimer();
+    document.getElementById("timer").innerHTML = timer;
+    document.getElementById("clicks").innerHTML = clicks;
+    if (clicked) {
+        document.getElementById("cps").innerHTML = parseFloat(clicks/5).toFixed(1);
+    } else {
+        document.getElementById("cps").innerHTML = 0;
+    }
+    requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
+setInterval(removeSec, 1000);
